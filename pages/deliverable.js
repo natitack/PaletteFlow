cde9b31dfd432ee77bc9b895903f28bd1f6dd679
natitack <114112085+natitack@users.noreply.github.com>
2025-02-10 17:13:58 +0000

    Add Deliverable page

---
+++ pages/deliverable.js
@@ -0,0 +1,17 @@
+import ColorPalette from '../components/ColorPalette'
+
+function Deliverable() {
+    return (
+        <main>
+            <h1>Deliverable</h1>
+            <p>
+                This is the deliverable for the first assignment of the course. It is a simple web application that displays a color palette
+            </p>
+            <hr />
+        ColorPalette.render()
+        
+        </main>
+    )
+}
+
+export default Deliverable
