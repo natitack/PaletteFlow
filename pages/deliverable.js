b8b209735f97255ae796ac8fd53bc23ee464a30a
bennetthamilton <114451107+bennetthamilton@users.noreply.github.com>
2025-02-13 19:35:11 +0000

    simplify deliverable page

--- pages/deliverable.js
+++ pages/deliverable.js
@@ -1,17 +1,9 @@
-import ColorPalette from '../components/ColorPalette copy'
 
-
-function Deliverable() {
+export default function Deliverable() {
     return (
-        <main>
-            <h1>Deliverable</h1>
-            <p>
-                This is the deliverable for the first assignment of the course. It is a simple web application that displays a color palette
-            </p>
-            <hr />
-            <ColorPalette />
-        </main>
-    )
-}
-
-export default Deliverable
+      <div>
+        <h1>Deliverable</h1>
+        <p>This page will generate the style guide for the client.</p>
+      </div>
+    );
+  }
